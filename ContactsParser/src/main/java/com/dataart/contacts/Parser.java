8f925a99914a85ef008fbd11b8aa44efa21638a3
Alexey Psarev <alexey.psarev@gmail.com>
2015-06-25 17:26:38 +0300

    Parser's and DataProcessor's methods signatures

--- ContactsParser/src/main/java/com/dataart/contacts/Parser.java
+++ ContactsParser/src/main/java/com/dataart/contacts/Parser.java
@@ -1,13 +1,30 @@
 package com.dataart.contacts;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.File;
+
+class Parser 
 {
-    public static void main( String[] args )
+	public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+		if (args.length != 2)
+		{
+			System.out.println("Usage: Parser <source file> <target file>\n");
+			return;
+		}
+		File inputFile = new File(args[0]);
+		File outputFile = new File(args[1]);
+		if (!(inputFile.getName().endsWith(".zip")) ||
+			!(outputFile.getName().endsWith(".zip")))
+		{
+			System.out.println("You should specify zip files\n");
+			return;
+		}
+		if (!(inputFile.exists()) || !(inputFile.isFile()))
+		{
+			System.out.println("Source file must exist and be a normal zip file\n");
+		}
+		
+		DataProcessor processor = new DataProcessor(inputFile, inputFile);
+		processor.process();
     }
 }
