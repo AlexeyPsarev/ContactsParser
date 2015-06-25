a39106510f54b98f2552a0f97562cb1cad998cb0
Alexey Psarev <alexey.psarev@gmail.com>
2015-06-25 16:35:52 +0300

    Created maven project

---
+++ ContactsParser/src/main/java/com/dataart/contacts/Parser.java
@@ -0,0 +1,13 @@
+package com.dataart.contacts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
