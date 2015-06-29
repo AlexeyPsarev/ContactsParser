04cce12698105e74d3ae53dcd7bc226453f67b28
Alexey Psarev <alexey.psarev@gmail.com>
2015-06-29 16:58:08 +0300

    Implemented zip decompressing and compressing

--- ContactsParser/src/main/java/com/dataart/contacts/Parser.java
+++ ContactsParser/src/main/java/com/dataart/contacts/Parser.java
@@ -6,25 +6,25 @@ class Parser
 {
 	public static void main(String[] args)
     {
-		if (args.length != 2)
+	/*	if (args.length != 2)
 		{
 			System.out.println("Usage: Parser <source file> <target file>\n");
 			return;
-		}
-		File inputFile = new File(args[0]);
-		File outputFile = new File(args[1]);
-		if (!(inputFile.getName().endsWith(".zip")) ||
-			!(outputFile.getName().endsWith(".zip")))
+		}*/
+		File inputFile = new File("inputs.zip");//args[0]);
+		File outputFile = new File("outputs.zip");//args[1]);
+		if (!(inputFile.getName().toLowerCase().endsWith(".zip")) ||
+			!(outputFile.getName().toLowerCase().endsWith(".zip")))
 		{
 			System.out.println("You should specify zip files\n");
 			return;
 		}
 		if (!(inputFile.exists()) || !(inputFile.isFile()))
 		{
-			System.out.println("Source file must exist and be a normal zip file\n");
+			System.out.println("Source file must exist and be a normal file\n");
 		}
 		
-		DataProcessor processor = new DataProcessor(inputFile, inputFile);
+		FileProcessor processor = new FileProcessor(inputFile, outputFile);
 		processor.process();
     }
 }
