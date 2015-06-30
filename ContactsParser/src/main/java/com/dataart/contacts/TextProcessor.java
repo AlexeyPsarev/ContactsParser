57789d8b1363918fbce1ea7c1fb749714c907928
Alexey Psarev <alexey.psarev@gmail.com>
2015-06-30 14:23:48 +0300

    Modificated text processor: phone is limited by email only; email must contain '@'; string might not contain email

--- ContactsParser/src/main/java/com/dataart/contacts/TextProcessor.java
+++ ContactsParser/src/main/java/com/dataart/contacts/TextProcessor.java
@@ -11,9 +11,8 @@ public class TextProcessor
 		emails = new TreeSet<String>();
 		phoneBegin = -1;
 		phoneEnd = -1;
-		emailBegin = -1;
 	}
-	
+
 	public String process(final String str)
 	{
 		String result;
@@ -22,70 +21,63 @@ public class TextProcessor
 		emailParse(result);
 		return result;
 	}
-	
+
 	public String getPhone()
 	{
 		return phone;
 	}
-	
+
 	public TreeSet<String> getEmails()
 	{
 		return emails;
 	}
-	
+
 	public void reset()
 	{
 		phone = "";
 		emails.clear();
 		phoneBegin = -1;
 		phoneEnd = -1;
-		emailBegin = -1;
 	}
-	
+
 	private String findAndEditPhone(final String str)
 	{
 		int indexOfAt = str.indexOf('@');
-		if (indexOfAt == -1)
+		phoneEnd = (indexOfAt != -1) ? str.lastIndexOf(' ', indexOfAt) : (str.length());
+		if (phoneEnd == -1)
 			return str;
-		int lastPhoneSpace = str.lastIndexOf(' ', indexOfAt);
-		if (lastPhoneSpace == -1)
-			return str;
-		emailBegin = lastPhoneSpace + 1;
-		phoneBegin = str.substring(0, lastPhoneSpace).indexOf('+');
+		phoneBegin = str.substring(0, phoneEnd).indexOf('+');
 		if (phoneBegin == -1)
 			return str;
-		
+
 		int cur = phoneBegin + 1;
-		int codeBegin = phoneBegin;
-		int codeEnd = phoneBegin;
-		boolean isPhoneFound = false;
-		while (!isPhoneFound && cur < lastPhoneSpace)
-		{			
+		int codeBegin = -1;
+		int codeEnd = -1;
+		boolean isCodeFound = false;
+		while (!isCodeFound && cur < phoneEnd)
+		{
 			char c = str.charAt(cur);
 			if (c == '(')
 				codeBegin = cur;
 			else if (c == ')')
 				codeEnd = cur + 1;
-			isPhoneFound = (!Character.isDigit(c)) && (c != ' ') && (c != '(') && (c != ')');
-			boolean[] arr = {
-				!Character.isDigit(c),
-				(c != ' '),
-				(c != '('),
-				(c != ')')
-			};
+			isCodeFound = (codeBegin != -1) && (codeEnd != -1);
 			++cur;
 		}
-		phoneEnd = cur;
 		
-		String code = str.substring(codeBegin, codeEnd);
-		code = code.replace("101", "401");
-		code = code.replace("202", "802");
-		code = code.replace("301", "321");
-		
-		return str.substring(0, codeBegin) + code + str.substring(codeEnd);
+		if (isCodeFound && (codeBegin < codeEnd))
+		{
+			String code = str.substring(codeBegin, codeEnd);
+			code = code.replace("(101)", "(401)");
+			code = code.replace("(202)", "(802)");
+			code = code.replace("(301)", "(321)");
+			return str.substring(0, codeBegin) + code + str.substring(codeEnd);
+		}
+		else
+			return str;		
 	}
-	
-	private void savePhone(String str)
+
+	private void savePhone(final String str)
 	{
 		StringBuilder builder = new StringBuilder();
 		char c;
@@ -97,20 +89,24 @@ public class TextProcessor
 		}
 		phone = builder.toString();
 	}
-	
+
 	private void emailParse(final String str)
 	{
-		if (emailBegin == -1)
+		if (phoneEnd == -1)
 			return;
 		StringTokenizer tokenizer =
-			new StringTokenizer(str.substring(emailBegin), " \t,;");
+			new StringTokenizer(str.substring(phoneEnd), " \t,;");
+		String token;
 		while (tokenizer.hasMoreTokens())
-			emails.add(tokenizer.nextToken());
+		{
+			token = tokenizer.nextToken();
+			if (token.contains("@"))
+				emails.add(token);
+		}
 	}
-	
+
 	private String phone;
 	private TreeSet<String> emails;
 	private int phoneBegin;
 	private int phoneEnd;
-	private int emailBegin;
 }
